e882c4a809efe270424bc7e4b93a474bf1dcfe9d
Isaac Garcia <garciaalejoisaac@gmail.com>
2024-09-23 21:34:27 -0500

    New Content

--- script.js
+++ script.js
@@ -60,6 +60,37 @@ function menu() {
                 console.log(ejercicio15());
                 break;
 
+            case 16:
+                console.log(ejercicio16());
+                break;    
+
+            case 17:
+                console.log(ejercicio17());
+                break;    
+            case 18:
+                console.log(ejercicio18());
+                break;    
+            case 19:
+                console.log(ejercicio19());
+                break;    
+            case 20:
+                console.log(ejercicio20());
+                break;    
+            case 21:
+                console.log(ejercicio21());
+                break;    
+            case 22:
+                console.log(ejercicio22());
+                break;    
+            case 23:
+                console.log(ejercicio23());
+                break;    
+            case 24:
+                console.log(ejercicio24());
+                break;    
+            case 25:
+                console.log(ejercicio25());
+                break;    
             
             default:
                 console.log('Opción no válida, por favor selecciona un número entre 1 y 11.');
@@ -193,6 +224,126 @@ function ejercicio15() {
 }
 
 console.log(ejercicio15()); // ['60', '80', '100']
+
+
+// ejercicio 16 
+function ejercicio16() {
+    let palabras = ['sol', 'luna', 'estrella', 'cometa'];
+
+    // Usamos find para buscar la primera palabra que contiene la letra 'a'
+    let resultado = palabras.find(p => p.includes('a'));
+
+    return resultado;
+}
+
+// Llamamos a la función para ver el resultado
+console.log(ejercicio16()); // Debería devolver 'estrella'
+
+
+// ejercicio 17
+
+function ejercicio17(){
+
+    let Suma = ['5','10','15','20']; 
+
+    let total = Suma.map(num => num + 5);
+
+    return total;
+}
+
+console.log(ejercicio17())
+
+// Ejercicio 18
+
+function ejercicio18(){
+    let nombre = ['CASA', 'ARBOL', 'SOL'];
+    let MayusNombre = nombre.map(n => n.toLowerCase());
+    console.log(MayusNombre);
+}
+
+console.log(ejercicio18())
+
+// Ejercicio 19 
+
+function ejercicio19(){
+    let pares = ['12','3','5','7','10','20','25'];
+    let ParesNumber = pares.filter(num => Number(num) % 2 === 0);
+    console.log(ParesNumber);
+}
+
+console.log(ejercicio19())
+
+// Ejercicio 20
+
+function ejercicio20(){
+let negativo = ['5','10','-3','2','-8','7'];
+let NumNegativo = negativo.find(num => Number(num) < 0);
+
+return NumNegativo
+
+}
+
+console.log(ejercicio20())
+
+// Ejercicio 21
+function ejercicio21(){
+let div = ['10','20','30','40'];
+let NewDiv = div.map(num => Number(num) / 2);
+return NewDiv
+}
+
+console.log(ejercicio21())
+
+// Ejercicio 22
+function ejercicio22(){
+
+    let letras = ['camino','casa','edificio','ciudad'];
+    let AnswerLetras = letras.filter(palabra => palabra.length > 6);
+
+    return AnswerLetras
+}
+
+console.log(ejercicio22())
+
+// Ejercicio 23
+
+function ejercicio23(){
+    let numero = ['4','9','13','22','31'];
+    let ImparNUmero = numero.find(num => Number(num) > 10 && Number(num) % 2 !== 0);
+
+    return ImparNUmero
+}
+
+console.log(ejercicio23())
+
+
+// Ejercicio 24 
+
+function ejercicio24(){
+    let element = ['manzana','naranja','pera',]
+    let NewElement = element.map(fruta => fruta + " fruta ");
+
+    return NewElement
+}
+
+console.log(ejercicio24())
+
+
+// Ejercicio 25 
+
+function ejercicio25() {
+    let MAyorNumber = [10, 20, 30, 40, 50, 60];
+    
+    // Calculamos el promedio
+    let promedio = MAyorNumber.reduce((acc, num) => acc + num, 0) / MAyorNumber.length;
+
+    // Usamos filter para devolver solo los números mayores que el promedio
+    let mayoresQuePromedio = MAyorNumber.filter(num => num > promedio);
+
+    return mayoresQuePromedio;
+}
+
+console.log(ejercicio25()); // Debería devolver [40, 50, 60]
 
 // Llama a la función menu
 menu();
